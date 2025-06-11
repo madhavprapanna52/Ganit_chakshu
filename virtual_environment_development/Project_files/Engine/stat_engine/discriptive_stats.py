e9702e5688fdb77ff0e6464e9d7643b8361badb3
madhavr <madhavprapanna@gmail.com>
2025-06-11 15:20:11 +0530

    Made very simple discriptive statistics tool with central tendency computation : able to get mean median mode

--- virtual_environment_development/Project_files/Engine/stat_engine/discriptive_stats.py
+++ virtual_environment_development/Project_files/Engine/stat_engine/discriptive_stats.py
@@ -30,9 +30,42 @@ class Discriptive_stat():
             
     '''
     def __init__(self, data_set):
-        self.data_set = data_set
+        self.data_set = data_set  # whole dataset into list 
 
-    def central_tendency(self):
+    def central_tendency(self, data_list):  # computation unit 
         '''
-        Making basic computations of central tendency of dataset 
+        Making basic computations of central tendency of dataset
+        mean , median mode computations based on data type : 
+            MVP : Only for numerical datasets for now 
         '''
+        # Making type conversion 
+        int_list = []
+        for i in data_list:
+            int_list.append(int(i))  # int converg
+        mean = statistics.mean(int_list)
+        median = statistics.median(int_list)
+        mode = statistics.mode(int_list)
+
+        del int_list
+        print(f"""
+        MEAN OF DATA : {mean}
+        MEDIAN OF DATA : {median}
+        MODE OF DATA : {mode}
+        """)  #TODO Make sujjester which gives best parameter to consider for the data 
+
+    def present(self):
+        """
+        Making presentation for the all collums 
+        iterating with row selection through dataset 
+        # TODO organised data strucures flow is must in project 
+        """
+        # fetch collums of numericals
+        numerical_data_collums = handle_data.row('n')  # data of all numerical dataset 
+        mark = 1   # simple marking 
+        for collums in numerical_data_collums: 
+            print(mark)
+            self.central_tendency(collums[1:])  # due to header exception
+            mark += 1
+
+discriptive_statistics = Discriptive_stat(data)  # loaded data into discriptive object 
+discriptive_statistics.present()
